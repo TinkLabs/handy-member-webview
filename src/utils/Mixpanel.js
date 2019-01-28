6884d6aeb3b67cde28902f30ede58e1fe2728b7a
Allen Wong <alnaym30@gmail.com>
2019-01-28 16:59:39 +0800

    try catch wrap mixpanel

--- src/utils/Mixpanel.js
+++ src/utils/Mixpanel.js
@@ -1,21 +1,25 @@
 import mixpanel from 'mixpanel-browser';
-import Android from './Android';
+import AndroidAPI from './AndroidAPI';
 
 
 export default {
 	trackScreenView: () => {
-		const properties = Android.getAndroidGlobalProperty();
-		if (!properties.device_user_id) return;
-		mixpanel.identify(properties.device_user_id);
-		mixpanel.track(
-			"Screen View",
-			Object.assign(Android.getAndroidGlobalProperty(), {
-				section: "my-account",
-				category: "index",
-				subcategory: "index",
-				screen_name: "my-account_index_index",
-				$user_id: properties.device_user_id,
-			})
-		);
+		try {
+			const properties = AndroidAPI.getAndroidGlobalProperty();
+			if (!properties.device_user_id) return;
+			mixpanel.identify(properties.device_user_id);
+			mixpanel.track(
+				"Screen View",
+				Object.assign(AndroidAPI.getAndroidGlobalProperty(), {
+					section: "my-account",
+					category: "index",
+					subcategory: "index",
+					screen_name: "my-account_index_index",
+					$user_id: properties.device_user_id,
+				})
+			);
+		} catch (e) {
+			console.error(e);
+		}
 	},
 };
